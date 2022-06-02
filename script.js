281ba853ac43144316bad377a62ce1750c042eba
kongguksu <104235938+kongguksu@users.noreply.github.com>
2022-06-02 11:38:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+///////// Defining variables
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+///////// Defining functions
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+///////// Events
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+// Keyboard event
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') closeModal();
+});
